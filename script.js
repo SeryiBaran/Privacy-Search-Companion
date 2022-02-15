189e582d18e8d89482d746d0630b5565a9d2c00b
Erghel <88831850+Erghel@users.noreply.github.com>
2022-02-16 00:41:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 function AnswerScrapper() {
     let answer = prompt("Введите ваш запрос", "");
 
-        document.getElementById("cr").innerHTML = `<a target="blank_" href="https://duckduckgo.com/?t=ffab&q=${answer}">Ответ</a>` + " - DuckDuckGo ";
-    //  document.getElementById("cc").innerHTML = `<a target="blank_" href="https://www.startpage.com/?q=${answer}">Ответ</a>`;
-        document.getElementById("cw").innerHTML = `<a target="blank_" href="https://swisscows.com/web?query=${answer}">Ответ</a>` + " - Swisscows";
-        document.getElementById("cq").innerHTML = `<a target="blank_" href="https://www.lukol.com/s.php?q=${answer}">Ответ</a>` + " - Lukol";
+        document.getElementById("ddg").innerHTML = `<a target="blank_" href="https://duckduckgo.com/?t=ffab&q=${answer}">Ответ</a>` + " - DuckDuckGo ";
+    //  document.getElementById("sp").innerHTML = `<a target="blank_" href="https://www.startpage.com/?q=${answer}">Ответ</a>`;
+        document.getElementById("sc").innerHTML = `<a target="blank_" href="https://swisscows.com/web?query=${answer}">Ответ</a>` + " - Swisscows";
+        document.getElementById("lk").innerHTML = `<a target="blank_" href="https://www.lukol.com/s.php?q=${answer}">Ответ</a>` + " - Lukol";
 
         console.log(`${answer}`);
     }

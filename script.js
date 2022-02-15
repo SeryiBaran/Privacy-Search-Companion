ff887ab8584ed73823f8b6b10b8e0cf06a62c7a3
Erghel <88831850+Erghel@users.noreply.github.com>
2022-02-16 01:21:55 +0300

    version 0.1

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 function AnswerScrapper() {
     let answer = prompt("Введите ваш запрос", "");
 
-        document.getElementById("ddg").innerHTML = `<a target="blank_" href="https://duckduckgo.com/?t=ffab&q=${answer}">Ответ</a>` + " - DuckDuckGo ";
-    //  document.getElementById("sp").innerHTML = `<a target="blank_" href="https://www.startpage.com/?q=${answer}">Ответ</a>`;
-        document.getElementById("sc").innerHTML = `<a target="blank_" href="https://swisscows.com/web?query=${answer}">Ответ</a>` + " - Swisscows";
-        document.getElementById("lk").innerHTML = `<a target="blank_" href="https://www.lukol.com/s.php?q=${answer}">Ответ</a>` + " - Lukol";
+        document.getElementById("ddg").innerHTML = `<a target="blank_" href="https://duckduckgo.com/?t=ffab&q=${answer}">Ответ</a>` + " - DuckDuckGo " + `<img src="https://cdn.iconscout.com/icon/free/png-64/duckduckgo-2752202-2285019.png">`;
+    //  document.getElementById("sp").innerHTML = `<a target="blank_" href="https://www.startpage.com/?q=${answer}">Ответ</a>` + " - Startpage" + `img src="https://cdn.iconscout.com/icon/free/png-64/magnifier-1439746-1214318.png">`;
+        document.getElementById("sc").innerHTML = `<a target="blank_" href="https://swisscows.com/web?query=${answer}">Ответ</a>` + " - Swisscows " + `<img src="https://cdn.iconscout.com/icon/free/png-64/cow-212-1115892.png">`;
+        document.getElementById("lk").innerHTML = `<a target="blank_" href="https://www.lukol.com/s.php?q=${answer}">Ответ</a>` + " - Lukol " + `<img src="https://cdn.iconscout.com/icon/free/png-64/hacker-14-448404.png">`;
 
         console.log(`${answer}`);
     }
